bb928fabc7c05c3be577a7f4c480720808776145
NishaJoshi666 <69041055+NishaJoshi666@users.noreply.github.com>
2021-04-19 11:20:50 +0530

    Delete boy.js

--- boy.js
+++
@@ -1,7 +0,0 @@
-class boy extends BaseClass{
-    constructor(x,y,height,angle){
-      super(x,y,150,height,angle);
-      this.image = loadImage("image/boy.png");
-      Matter.Body.setAngle(this.body, angle);
-    }
-  }
